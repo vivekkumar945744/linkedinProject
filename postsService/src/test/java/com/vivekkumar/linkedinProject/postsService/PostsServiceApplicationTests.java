7b4cce4c54428a3d2530aa0f6a5a5f4141e920ca
vivekkumar945744 <vivekkumar945744@gmail.com>
2026-05-14 19:57:09 +0530

    first commit

---
+++ postsService/src/test/java/com/vivekkumar/linkedinProject/postsService/PostsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekkumar.linkedinProject.postsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ userService/src/test/java/com/vivekkumar/linkedinProject/userService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekkumar.linkedinProject.userService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
